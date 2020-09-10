02bf6afbee5303802c8d555cc65493c884660f66
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2020-09-10 14:36:14 +0200

    gui coloring and symbols formatting

--- gui/UI.py
+++ gui/UI.py
@@ -1,3 +1,7 @@
+import tkinter
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+
 from card_detector.classes.card_rank_enum import CardRank
 from card_detector.classes.card_suit_enum import CardSuit
 from card_detector.classes.poker_card import PokerCard
@@ -7,56 +11,103 @@ from visualization.classes.player import Player
 from chip_detector.classes.poker_chip_enum import PokerChip
 from tkinter import *
 
+root = tkinter.Tk()
+root.title('PokeVisor')
+
+
+def card_rank_formatter(card):
+    if card.rank.name == 'two':
+        return '2'
+    elif card.rank.name == 'three':
+        return '3'
+    elif card.rank.name == 'four':
+        return '4'
+    elif card.rank.name == 'five':
+        return '5'
+    elif card.rank.name == 'six':
+        return '6'
+    elif card.rank.name == 'seven':
+        return '7'
+    elif card.rank.name == 'eight':
+        return '8'
+    elif card.rank.name == 'nine':
+        return '9'
+    elif card.rank.name == 'jack':
+        return 'J'
+    elif card.rank.name == 'queen':
+        return 'Q'
+    elif card.rank.name == 'king':
+        return 'K'
+    else:
+        return 'A'
+
+
+def card_suit_formatter(card):
+    if card.suit.name == 'spades':
+        return chr(9824)
+    elif card.suit.name == 'diamonds':
+        return chr(9830)
+    elif card.suit.name == 'hearts':
+        return chr(9829)
+    else:
+        return chr(9827)
+
 
 class TUI:
     @staticmethod
     def write__turn(community_cards, list_of_players):
-        community_cards_row1 = Entry(relief=RIDGE, width=70)
-        community_cards_row1.grid(row=1, column=1, sticky=NSEW)
-        community_cards_row1.insert(END, "Community Cards")
+        community_cards_column1 = Entry(root, width=50)
+        community_cards_column1.config({"background": "#97d8bc", "font": "bold"})
+        community_cards_column1.grid(row=1, column=1, sticky=NSEW)
+        community_cards_column1.insert(END, "Community Cards")
 
-        community_cards_row2 = Entry(relief=RIDGE, width=70)
-        community_cards_row2.grid(row=1, column=2, sticky=NSEW)
+        community_cards_column2 = Entry(root, width=50)
+        community_cards_column2.config({"background": "#dcf2e8", "font": "bold"})
+        community_cards_column2.grid(row=1, column=2, sticky=NSEW)
         if len(community_cards) == 0:
-            community_cards_row2.insert(END, "Preflop")
+            community_cards_column2.insert(END, "Preflop")
             print("preflop")
         elif len(community_cards) == 3:
-            community_cards_row2.insert(END, "Flop")
+            community_cards_column2.insert(END, "Flop")
             print("flop")
             print("community cards")
         elif len(community_cards) == 4:
-            community_cards_row2.insert(END, "Turn")
+            community_cards_column2.insert(END, "Turn")
             print("turn")
             print("community cards")
         elif len(community_cards) == 5:
-            community_cards_row2.insert(END, "River")
+            community_cards_column2.insert(END, "River")
             print("river")
             print("community cards")
         cards_to_print = ""
         for card in community_cards:
-            cards_to_print += card.rank.name + " " + card.suit.name + ", "
+            cards_to_print += card_rank_formatter(card) + card_suit_formatter(card) + ", "
             print(card.rank.name + " " + card.suit.name)
-        community_cards_row3 = Entry(relief=RIDGE, width=70)
-        community_cards_row3.grid(row=1, column=3, sticky=NSEW)
-        community_cards_row3.insert(END, cards_to_print)
+        community_cards_column3 = Entry(root, width=50)
+        community_cards_column3.config({"background": "#dcf2e8", "font": "bold"})
+        community_cards_column3.grid(row=1, column=3, sticky=NSEW)
+        community_cards_column3.insert(END, cards_to_print)
 
         for player in list_of_players:
-            player_column1 = Entry(relief=RIDGE, width=70)
-            player_column1.grid(row=list_of_players.index(player)+2, column=1, sticky=NSEW)
+            player_column1 = Entry(root, width=50)
+            player_column1.config({"background": "#dcf2e8", "font": "bold"})
+            player_column1.grid(row=list_of_players.index(player) + 2, column=1, sticky=NSEW)
             player_column1.insert(END, player.name)
 
             print(player.name + "\n Cards:")
             cards = ""
             for card in player.cards:
-                cards += card.rank.name + " " + card.suit.name + ", "
+                cards += card_rank_formatter(card) + card_suit_formatter(card) + ", "
                 print(card.rank.name + " " + card.suit.name)
 
-            player_column2 = Entry(relief=RIDGE)
-            player_column2.grid(row=list_of_players.index(player)+2, column=2, sticky=NSEW)
+            player_column2 = Entry(root)
+            player_column2.config({"background": "#ffffff", "font": "bold"})
+            player_column2.grid(row=list_of_players.index(player) + 2, column=2, sticky=NSEW)
             player_column2.insert(END, cards)
 
-            player_column3 = Entry(relief=RIDGE)
-            player_column3.grid(row=list_of_players.index(player)+2, column=3, sticky=NSEW)
+            player_column3 = Entry(root)
+            player_column3.config({"background": "#ffffff", "font": "bold"})
+            player_column3.grid(row=list_of_players.index(player) + 2, column=3, sticky=NSEW)
             player_column3.insert(END, "has {} value in chips".format(player.sum_chip_values()))
 
             print("{} has {} value in chips".format(player.name, player.sum_chip_values()))
